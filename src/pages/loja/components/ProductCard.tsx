e1c33957a6909128d14f9ad1342badf7a67637ac
jvadebossan <jvdebossan@gmail.com>
2025-05-31 20:02:16 -0300

    Fix: set product card min size and fix style

--- src/pages/loja/components/ProductCard.tsx
+++ src/pages/loja/components/ProductCard.tsx
@@ -18,20 +18,22 @@ export default function ProductCard({ product }: ProductCardProps) {
       key={product.Nome}
       className="border-2 border-neutral-200 shadow-accent rounded-lg p-3 w-full max-w-sm bg-transparent"
     >
-      <CardContent className="flex justify-start items-center p-0">
+      <CardContent className="flex justify-start items-center p-0 min-h-[100px]">
         <img
           src={product.ImagemUrl}
           alt="Product"
-          className="w-[32%] h-auto object-cover rounded-md aspect-square"
+          className="w-24 h-24 object-cover rounded-md flex-shrink-0"
         />
-        <div className="pl-4 flex-col justify-center items-start w-full h-auto">
-          <div className="flex-col justify-start items-start w-full h-full">
-            <h3 className="text-lg font-semibold text-neutral-800">
+        <div className="pl-4 flex flex-col justify-between items-start w-full h-24">
+          <div className="flex flex-col justify-start items-start w-full">
+            <h3 className="text-lg font-semibold text-neutral-800 line-clamp-1">
               {product.Nome}
             </h3>
-            <p className="text-sm text-neutral-600">{product.Descricao}</p>
+            <p className="text-sm text-neutral-600 line-clamp-2">
+              {product.Descricao}
+            </p>
           </div>
-          <div className="flex justify-start items-center">
+          <div className="flex justify-start items-center mt-2">
             <Badge
               variant="default"
               className="bg-green-100 text-green-600 shadow-xs text-xs rounded-full font-semibold"
